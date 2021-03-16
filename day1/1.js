ca680bd11bb9a767b2b65e162dc0d3962c1ef379
JoohyeongPark <pjh603@gmail.com>
2021-03-16 21:51:27 +0900

    0316_before_go_home

--- day1/1.js
+++ day1/1.js
@@ -1,5 +1,13 @@
 /*
+
 1번
+What is your name? 
+-> Park
+Hello, Park, nice to meet you!
+
+입력부부분, 문자열 연결(String concatenation), 출력을 별도로
+
+
 */
 
 alert('안녕하세요 '+ prompt('이름이 무엇인가요')+' 님 반갑습니다.')
@@ -23,6 +31,9 @@ function f(name){
 f(prompt('What is your name'));
 
 
-
+//도전과제
 //변수를 사용하지 않는 새로운 버전
-alert('안녕 '+prompt('이름이 무엇인가요?').concat(' ','반가워'))
+alert('안녕 '+prompt('이름이 무엇인가요?').concat(' ','반가워'))\
+
+//도전과제
+//이름에따라 다른 인사말이 나오도록
--- day1/2.js
+++ day1/2.js
@@ -1,7 +1,47 @@
 /*
 2번
+
+What is the input string? Park
+
+Park has 5 characters
+
 */
 
+//출력결과는 입력받은 문자열이 그대로 나타나도록 할 것
+//하나의 출력문만 사용할 것
+//언어에서 제공하는 내장함수 사용
 var strName;
- strName = prompt("What is the input string?")
+strName = prompt('What is the input string?');
+alert(strName+'은(는) ' +strName.length+'자 입니다.');
 
+//도전과제
+//사용자가 아무것도 입력하지않았을 때 엔터키를 치면 입력하라는 메세지를 나타내기
+
+var strName;
+strName = prompt('What is the input string?');
+if(strName.length==0){
+    alert("공백입니다.")
+
+} else{
+    alert(strName+'은(는) ' +strName.length+'자 입니다.');
+}
+
+
+
+
+//도전과제
+//GUI버전 실시간 업데이트기능만들기 HTML활용가능
+
+//step1
+function cntStr(){
+    var rtn;
+    if(strName.length==0){
+        rtn = "공백입니다.";
+        return rtn;
+    } else{
+        rtn =  strName+'은(는) ' +strName.length+'자 입니다.';
+        return rtn;
+    }
+}
+
+cntStr(prompt('What is the input string?'))
