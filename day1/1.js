6bc24107d6f496284f07d29d5c291065e8f9419f
JoohyeongPark <pjh603@gmail.com>
2021-03-16 17:53:42 +0900

    4th commit

--- day1/1.js
+++ day1/1.js
@@ -1,5 +1,10 @@
+/*
+1번
+*/
+
 alert('안녕하세요 '+ prompt('이름이 무엇인가요')+' 님 반갑습니다.')
 
+//출력부분을 별도로 설정
 function f(name){
     var a;
     a = 'Hi! '+name+ ', nice to meet you!';
@@ -8,6 +13,7 @@ function f(name){
 }
 f(prompt('What is your name'));
 
+//문자열 연결(String concatenation)기능 사용
 function f(name){
     var a;
     a = '안녕';
@@ -18,5 +24,5 @@ f(prompt('What is your name'));
 
 
 
-
+//변수를 사용하지 않는 새로운 버전
 alert('안녕 '+prompt('이름이 무엇인가요?').concat(' ','반가워'))
---
+++ day1/2.js
@@ -0,0 +1,7 @@
+/*
+2번
+*/
+
+var strName;
+ strName = prompt("What is the input string?")
+
