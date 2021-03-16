11a996f28763fa823506d719dce3fcf0a59800a9
JoohyeongPark <pjh603@gmail.com>
2021-03-16 15:30:39 +0900

    third commit

--- day1/1.js
+++ day1/1.js
@@ -1,1 +1,22 @@
-alert("안녕하세요 "+ prompt("이름이 무엇인가요")+" 님 반갑습니다.")
+alert('안녕하세요 '+ prompt('이름이 무엇인가요')+' 님 반갑습니다.')
+
+function f(name){
+    var a;
+    a = 'Hi! '+name+ ', nice to meet you!';
+    alert(a);
+    return a;
+}
+f(prompt('What is your name'));
+
+function f(name){
+    var a;
+    a = '안녕';
+    alert(a.concat(', ',name,' 반가워'));
+    //return a;
+}
+f(prompt('What is your name'));
+
+
+
+
+alert('안녕 '+prompt('이름이 무엇인가요?').concat(' ','반가워'))
